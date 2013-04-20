508740394ed345dbcee2eae4a260e7fcc092ebbc
Stephen Irons <stephen.irons@clear.net.nz>
2013-04-20 13:50:08 +1200

    Changed to different IP address.

--- ip9258-test.lua
+++ ip9258-test.lua
@@ -5,6 +5,9 @@ ip9258 = require 'ip9258'
 switch = arg[1]
 state = arg[2]
 
-sw = ip9258.new('172.16.163.209', '80', 'admin', '12345678', switch)
-sw:set(state)
+sw1 = ip9258.new('172.16.163.208', '80', 'admin', '12345678', switch)
+sw1:set(state)
+
+sw2 = ip9258.new('172.16.163.209', '80', 'admin', '12345678', switch)
+-- sw2:set(state)
 

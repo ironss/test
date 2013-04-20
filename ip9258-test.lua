b6b628a2d630ece62c9ac898e21028e792847464
Stephen Irons <stephen.irons@clear.net.nz>
2013-04-20 23:30:47 +1200

    Pass in IP address and port on the command line.

--- ip9258-test.lua
+++ ip9258-test.lua
@@ -2,7 +2,10 @@
 
 ip9258 = require 'ip9258'
 
-d1 = ip9258.new('172.16.163.209', '80', 'admin', '12345678')
+ip = arg[1] or '172.16.163.209'
+port = arg[2] or '80'
+
+d1 = ip9258.new(ip, port, 'admin', '12345678')
 d1:beeper(0)
 
 state = 1
